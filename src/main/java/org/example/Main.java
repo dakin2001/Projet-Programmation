e6e37aa2ee0cea4dfafa8c7bb93707fcf15e1a8f
Derrick <145935725+dakin2001@users.noreply.github.com>
2026-05-28 09:55:03 +0200

    up to date the main folder

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,31 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
+import javax.swing.JFrame;
 
-    }
+/**
+ * 
+ * Classe principale du jeu
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+
+		//Fen�tre de lancement du jeu
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.setTitle("ESIR1 - Projet Prog");
+		
+		//Ajout du panel du jeu et d�marrage du thread principal
+		GamePanel gamePanel = new GamePanel();
+		window.add(gamePanel);
+		window.pack();
+		
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+		gamePanel.startGameThread();	
+
+	}
+
 }
